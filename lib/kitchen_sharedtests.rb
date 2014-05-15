1fb1441ccffe98ab2194fa4a50f362d79f8f0556
Edmund Haselwanter <me@ehaselwanter.com>
2014-05-15 20:44:48 +0200

    shared repo is for integration tests only

--- lib/kitchen_sharedtests.rb
+++ lib/kitchen_sharedtests.rb
@@ -4,6 +4,7 @@ module Kitchen
   module Sharedtests
     TEST_REPO_NAME = "shared_test_repo"
     TEST_REPO_BASE_PATH = File.join(Dir.pwd, TEST_REPO_NAME)
-    TEST_BASE_PATH = File.join(TEST_REPO_BASE_PATH, "/integration")
+    # this is the path to the directory containing the integration test suites
+    TEST_BASE_PATH = TEST_REPO_BASE_PATH
   end
 end
